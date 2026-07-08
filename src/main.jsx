525c3ce8b2c8df9687a77fe8f7674210220cf3fe
leksatareum <a.valery@hotmail.fr>
2026-07-09 01:13:02 +0200

    Remove logo polish imports from main.jsx
    Removed logo polish CSS and JS imports.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './logo-polish.css'
-import { applyLogoPolish } from './logo-polish.js'
+
 
 createRoot(document.getElementById('root')).render(<App />)
 applyLogoPolish()
