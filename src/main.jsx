77180177a120df55e1455017038e41d1bfaeef35
leksatareum <a.valery@hotmail.fr>
2026-07-08 23:32:31 +0200

    Wire logo polish module

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './logo-polish.css'
+import { applyLogoPolish } from './logo-polish.js'
 
 createRoot(document.getElementById('root')).render(<App />)
+applyLogoPolish()
